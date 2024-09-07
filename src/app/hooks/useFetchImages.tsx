af88c99a8efdcdb0bcbecccfed071c56e17324c9
mselmank <m.selmankunstmann@gmail.com>
2024-09-07 16:34:19 -0400

    Fix: useFetchImages with Swr

--- src/app/hooks/useFetchImages.tsx
+++ src/app/hooks/useFetchImages.tsx
@@ -2,12 +2,13 @@ import { Key } from "react";
 import useSWR from "swr";
 
 const useFetchImages = () => {
-  const url = "https://challenge-uno.vercel.app/api/images#";
+  const url = "https://challenge-uno.vercel.app/api/images";
 
   const fetcher = (url: string | URL | Request) =>
     fetch(url).then((res) => res.json());
 
   const { data, error, isLoading } = useSWR(url, fetcher);
+  // console.log("🚀 ~ useFetchImages ~ data:", data);
 
   if (error) return <div>An error occurred while fetching images.</div>;
   if (isLoading) return <div>''</div>;
