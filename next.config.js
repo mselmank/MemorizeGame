94d41cadd99b2b5f0953d6ecf2120ef2cb2f83fb
mselmank <m.selmankunstmann@gmail.com>
2024-09-09 21:53:49 -0300

    Fix: ESlint ignoreDuringBuilds 3rd

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
-export default {
+module.exports = {
   eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 };
